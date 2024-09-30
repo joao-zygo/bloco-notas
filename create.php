d8b66f196854deebba1a2d394db7d68729fdf625
joao-zygo <joao-vitor_bento@estudante.sesisenai.org.br>
2024-09-30 08:59:08 -0300

    create

--- create.php
+++ create.php
@@ -1,3 +1,28 @@
 <?php
+include 'db.php';
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $categoria = $_POST['categoria'];
+    $conteudo = $_POST['conteudo'];
+    $titulo = $_POST['titulo'];
+
+    $sql = "INSERT INTO notas (categoria, conteudo,titulo) VALUES ('$categoria', '$conteudo', '$titulo')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Novo registro criado com sucesso";
+    } else {
+        echo "Erro: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
 ?>
+
+<form method="post" action="create.php">
+    categoria: <input type="text" name="categoria" required>
+    conteudo: <input type="text" name="conteudo" required>
+    título: <input type="text" name="titulo" required>
+    <input type="submit" value="Adicionar">
+</form>
+
+<a href="read.php">Ver registros.</a>
