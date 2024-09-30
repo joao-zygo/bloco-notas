98077a35c28991e0b69598a2048338d6e7ae2348
JoHE64 <jonas_henchel@estudante.sesisenai.org.br>
2024-09-30 09:19:08 -0300

    teste

--- read.php
+++ read.php
@@ -1,5 +1,36 @@
 <?php
+    include 'db.php';
 
+    $sql = "SELECT * FROM notas";
+
+    $result = $conn -> query($sql);
+
+    if ($result -> num_rows > 0){
+        echo "<table border='1'>
+        <tr>
+            <th> ID </th>
+            <th> TITULO </th>
+            <th> CONTEUDO </th>
+            <th> CATEGORIA </th>
+        
+        </tr>";
+        while($row = $result -> fetch_assoc()){
+            echo "<tr>
+                <td> {$row['id_notas']} </td>
+                <td> {$row['titulo']} </td>
+                <td> {$row['conteudo']} </td>
+                <td> {$row['categoria']} </td>
+                <td>
+                <a href='update.php?id={$row['id_notas']}'>Editar</a> |
+                <a href='delete.php?id={$row['id_notas']}'>Excluir</a>
+                </td>
+                </tr>";
+        }
+        echo "</table>";
+    }else {
+        echo "Nenhum registro encontrado!!!";
+    }
+    
 ?>
 
 <!DOCTYPE html>
